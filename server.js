7ed031f1bdd0984254a350e666dff210164f1131
Gabriele <gabrielefurlancavalcanti.com>
2025-10-09 09:54:02 -0300

    ATIV01

---
+++ server.js
@@ -0,0 +1,12 @@
+// 1. Crie um projeto que contenha uma rota POST /alunos que receba:
+// {
+//   "nome": "Ana",
+//   "notas": [8, 7, 9, 6]
+// }
+// Calcular a média e retornar um JSON com nome, média e situação (APROVADO ou REPROVADO).
+// Considere acima de 6 para aprovado.
+
+
+
+
+
