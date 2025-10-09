cdcdbc9845dd3dd2ea7806342bdd73c0f0021ac5
Gabriele <gabrielefurlancavalcanti.com>
2025-10-09 09:55:36 -0300

    ATIV03

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
-// 2. Crie um projeto que contenha uma rota POST /soma e receba uma quantidade indefinida de números através do body e realize a soma dos valores: 
-// Verifique se os valores são numéricos antes de realizar o cálculo, caso não seja informa ao usuário e não realizar o cálculo.
+// 3. Crie um projeto que contenha uma rota POST /soma e receba uma quantidade indefinida de números através do body e realize a soma dos valores: 
+// Verifique se os valores são numéricos antes de realizar o cálculo, ignore os não númericos, some os demais..
 // Utilize a função reduce, pesquise em fontes na internet a forma de utilizar.
