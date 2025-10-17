b452bb05162dc4890893242620cc812314dc4bcb
Gabriele <gabrielefurlancavalcanti.com>
2025-10-17 08:19:02 -0300

    ATIV02

--- server.js
+++ server.js
@@ -12,14 +12,13 @@ async function validaNumeros(pNumeros) {
             throw new Error("Valor inválido");
         }
     }
-    return pNumeros
 }
 
 async function soma(pNumeros) {
     try {
-        const numeros = await validaNumeros(pNumeros);
-        const soma = numeros.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0); //'0' é o valor inicial do acumulador
-        return soma
+        await validaNumeros(pNumeros);
+        const soma = pNumeros.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0); //'0' é o valor inicial do acumulador
+        return soma;
     } catch (error) {
         throw new Error(`Erro ao realizar a operação: ${error.message}`);
     }
